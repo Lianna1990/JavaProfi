8e7461d6fe78acc93baa393b6668ff30b25619ef
Lianna1990 <egoian.lianna@mail.ru>
2019-02-28 12:14:05 +0300

    Отправка первого домашнего задания

---
+++ src/ru/homeworks/HomeWork1/Orange.java
@@ -0,0 +1,10 @@
+package ru.homeworks.HomeWork1;
+
+public class Orange implements Fruit {
+    Float weight = 1.0f;
+
+    @Override
+    public Float getWeight() {
+        return weight;
+    }
+}
